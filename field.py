a65ac73d9369ab157a4f55edcd496611ae746ec0
Normann25 <nan25-12@hotmail.dk>
2024-02-26 13:48:04 +0100

    edit

--- field.py
+++ field.py
@@ -7,7 +7,7 @@ import numpy as np
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 import os, sys
 #%%
-def read_data(path, parent_path):
+def read_testdata(path, parent_path):
     parentPath = os.path.abspath(parent_path)
     if parentPath not in sys.path:
         sys.path.insert(0, parentPath)
@@ -34,7 +34,28 @@ def read_data(path, parent_path):
     
     return data_dict
 #%%
-def plot_overview(ax, df):
+def read_csv(path, parent_path):
+    parentPath = os.path.abspath(parent_path)
+    if parentPath not in sys.path:
+        sys.path.insert(0, parentPath)
+    
+    files = os.listdir(path)
+    data_dict = {}
+
+    for file in files:
+        name = file.split('.')[0]
+        with open(os.path.join(path, file)) as f:
+            df = pd.read_csv(f)
+            df = df.dropna()
+
+            df['Time'] = pd.to_timedelta(df['t_base'].str.split().str[1]).astype('timedelta64[s]')
+            df = df.set_index('Time')
+
+        data_dict[name] = df
+    
+    return data_dict
+#%%
+def plot_test(ax, df):
     # ax.plot(df.index, df['Total ion current'], lw = 1, label = 'Total ion current')
     ax.plot(df.index, df['Sum'], lw = 1, label = 'Summed columns')
 
@@ -42,4 +63,14 @@ def plot_overview(ax, df):
     ax.xaxis.set_major_formatter(formatter)
 
     ax.legend(frameon = False, fontsize = 8)
+    ax.set(ylabel = 'Intensity', xlabel = 'Time')
+#%%
+def plot_overview(ax, df, ncol):
+    for key in df.keys()[1:]:
+        ax.plot(df.index, df[key], lw = 1, label = key)
+
+    formatter = FuncFormatter(lambda s, x: time.strftime('%H:%M', time.gmtime(s)))
+    ax.xaxis.set_major_formatter(formatter)
+
+    ax.legend(frameon = False, fontsize = 8, ncol = ncol)
     ax.set(ylabel = 'Intensity', xlabel = 'Time')
