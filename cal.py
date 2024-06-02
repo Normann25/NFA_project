90acbb3be4143c4dd43e08de6fe92f1b4e3c5492
Normann25 <nan25-12@hotmail.dk>
2024-06-02 22:58:22 +0200

    Update

--- cal.py
+++ cal.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from iminuit import Minuit
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import os, sys
@@ -74,3 +75,75 @@ def plot_MS(ax, df, key, width):
     ax.tick_params(axis = 'both', which = 'major', direction = 'out', bottom = True, left = True, labelsize = 8)
     ax.tick_params(axis = 'both', which = 'minor', direction = 'out', width = 1, length = 2, bottom = True, left = True)
     ax.yaxis.offsetText.set_fontsize(9)
+
+def linear_fit(x, y, a_guess, b_guess):
+
+    Npoints = len(y)
+
+    def fit_func(x, a, b):
+        return b + (a * x)
+
+    def least_squares(a, b) :
+        y_fit = fit_func(x, a, b)
+        squares = np.sum((y - y_fit)**2)
+        return squares
+    least_squares.errordef = 1.0    # Chi2 definition (for Minuit)
+
+    # Here we let Minuit know, what to minimise, how, and with what starting parameters:   
+    minuit = Minuit(least_squares, a = a_guess, b = b_guess)
+
+    # Perform the actual fit:
+    minuit.migrad();
+
+    # Extract the fitting parameters:
+    a_fit = minuit.values['a']
+    b_fit = minuit.values['b']
+
+    Nvar = 2                     # Number of variables 
+    Ndof_fit = Npoints - Nvar    # Number of degrees of freedom = Number of data points - Number of variables
+
+    # Get the minimal value obtained for the quantity to be minimised (here the Chi2)
+    squares_fit = minuit.fval                          # The chi2 value
+
+    # Calculate R2
+    def simple_model(b):
+        return b
+
+    def least_squares_simple(b) :
+        y_fit = simple_model(b)
+        squares = np.sum((y - y_fit)**2)
+        return squares
+    least_squares_simple.errordef = 1.0    # Chi2 definition (for Minuit)
+
+    # Here we let Minuit know, what to minimise, how, and with what starting parameters:   
+    minuit_simple = Minuit(least_squares_simple, b = b_guess)
+
+    # Perform the actual fit:
+    minuit_simple.migrad();
+
+    # Get the minimal value obtained for the quantity to be minimised (here the Chi2)
+    squares_simple = minuit_simple.fval                          # The chi2 value
+
+    R2 = 1 - (squares_fit / squares_simple)
+
+    # Print the fitted parameters
+    print(f"Fit: a={a_fit:6.6f}  b={b_fit:5.3f}  R2={R2:6.6f}")
+    
+    return a_fit, b_fit, squares_fit, Ndof_fit, R2
+
+def plot_with_LinReg(ax, data_dict, df_keys, a_guess, b_guess, lbl, clr, ax_labels):
+    for i, key in enumerate(data_dict.keys()):
+        df = data_dict[key]
+        x, y = df[df_keys[i][0]], df[df_keys[i][1]]
+
+        lbl_fit = lbl[i] + ' fit'
+
+        a, b, squares, ndof, R2 = linear_fit(x, y, a_guess[i], b_guess[i])
+        y_fit = a*x + b
+
+        ax.plot(x, y_fit, label = lbl_fit, color = clr[i], lw = 1.2)
+        ax.scatter(x, y, label = lbl[i], color = clr[i], s = 10)
+
+    ax.legend(fontsize = 8)
+    ax.tick_params(axis = 'both', which = 'major', direction = 'out', bottom = True, left = True, labelsize = 8)
+    ax.set(xlabel = ax_labels[0], ylabel = ax_labels[1])
